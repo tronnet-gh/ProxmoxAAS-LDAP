04e8f0cac3e90632f105316b5c8a07e4d4366fec
Arthur Lu <alu@tronnet.net>
2024-01-26 03:31:00 +0000

    add starttls init script, fix some bugs with init script

--- openldap/init.sh
+++ openldap/init.sh
@@ -1,3 +1,8 @@
+# PAAS LDAP openldap server initialization script
+# initializes a blank openldap server using root external bind
+# requires user input for base dn, admin user, and admin user password
+# requires slapd ldap-util
+
 export BASE_DN=''
 export ADMIN_ID=''
 export ADMIN_EMAIL=''
@@ -24,11 +29,6 @@ if [ "$ADMIN_PASSWD" = "$CONFIRM_PASSWD" ]; then
     sudo ldapmodify -H ldapi:/// -Y EXTERNAL -f pass.ldif
     sudo ldapadd -H ldapi:/// -Y EXTERNAL -c -f init.ldif
 
-    unset BASE_DN
-    unset ADMIN_ID
-    unset ADMIN_CN
-    unset ADMIN_SN
-    unset ADMIN_PASSWD
     rm auth.ldif init.ldif pass.ldif
 
 else 
@@ -36,3 +36,9 @@ else
     echo "Error: Passwords do not match."
 
 fi
+
+unset BASE_DN
+unset ADMIN_ID
+unset ADMIN_CN
+unset ADMIN_SN
+unset ADMIN_PASSWD
